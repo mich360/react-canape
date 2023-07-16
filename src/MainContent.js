a3139a7d807928991400485e4671b2adc0e41913
mich360 <fghbg751@yhoo.co.jp>
2023-07-16 18:04:53 +0900

    Add css

--- src/MainContent.js
+++ src/MainContent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './main.css'
 
 function MainContent() {
   const mainContentStyle = {
@@ -24,56 +25,3 @@ function MainContent() {
 }
 
 export default MainContent;
-
-// import React from 'react';
-
-// function MainContent() {
-//     const mainContentStyle = {
-//         /* スタイルの定義 */
-        
-//     #canape-button {
-//     width: 100px;
-//     height: 27px;
-//     font-size: 14px;
-//     background-color: #0000CC;
-//     font-family: cursive; 
-//     color: #ffff80;
-//     border-style: solid;
-//     border-color: #ffff80;
-//     border-width: 2px;
-//     border-radius: 8px;
-    
-//     }
-   
-//       };
-    
-//   return (
-//     <main>
-//       <h2>Main Content Component</h2>
-//       <p>This is the main content of the application.</p>
-//       <input id="canape-button" type="button" onclick="location.href='https://canape2020.stars.ne.jp/script/index.html'" value="戻る"></input>
-//     </main>
-//   );
-// }
-
-// export default MainContent;
-
-// 
-// MainContent.js
-
-// import React from 'react';
-
-// function MainContent() {
-//   const mainContentStyle = {
-//     /* スタイルの定義 */
-//   };
-
-//   return (
-//     <div style={mainContentStyle}>
-//       {/* コンテンツ */}
-//     </div>
-//   );
-// }
-
-// export default MainContent;
-
